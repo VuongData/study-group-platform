d64f783659dc19f09aa15d227d8737b30a92923a
Vuong <tranthienvuong71@gmail.com>
2026-01-07 10:20:41 +0700

    1020-07

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   
-  // 👇 THÊM ĐOẠN NÀY
   css: {
     preprocessorOptions: {
       scss: {
-        // Cách A: Chuyển sang trình biên dịch hiện đại (nhanh hơn & hết lỗi)
         api: 'modern-compiler', 
-        
-        // HOẶC Cách B: Nếu cách A lỗi, hãy dùng dòng dưới để tắt cảnh báo
-        // silenceDeprecations: ["legacy-js-api"],
       },
     },
   },
